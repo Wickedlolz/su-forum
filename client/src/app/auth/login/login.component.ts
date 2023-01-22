278c6854aea1e6cfd637d2487b82e35362b4d29a
Wickedlolz <iceshotgg.linux@gmail.com>
2023-01-22 09:55:17 +0200

    add resources part 3

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/core/services/user.service';
 
@@ -8,7 +14,16 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router) {}
+  loginFormGroup: FormGroup = this.formBuilder.group({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  });
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   handleLogin(): void {
     this.userService.login();
