9916579437471f150b7f967ea9cd9c9f79b56c14
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-01-28 13:16:24 +0200

    add login function

--- client/src/app/core/services/user.service.ts
+++ client/src/app/core/services/user.service.ts
@@ -1,21 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IUser } from '../interfaces/user';
+
+interface IUserLoginDto {
+  email: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   isLoggedIn: boolean = false;
+  user: IUser | null = null;
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  login(): void {
-    this.isLoggedIn = true;
+  login(userData: IUserLoginDto): Observable<IUser> {
+    return this.http.post<IUser>('http://localhost:3000/api/login', userData, {
+      withCredentials: true,
+    });
   }
 
   logout(): void {
     this.isLoggedIn = false;
+    this.user = null;
     this.router.navigate(['/home']);
   }
 }
