8c5c2faa441628062ac0aca531913c8337f6aa12
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-01-28 13:26:21 +0200

    format

--- rest-api/controllers/auth.js
+++ rest-api/controllers/auth.js
@@ -1,42 +1,46 @@
-const {
-    userModel,
-    tokenBlacklistModel
-} = require('../models');
+const { userModel, tokenBlacklistModel } = require('../models');
 
 const utils = require('../utils');
 const { authCookieName } = require('../app-config');
 
-const bsonToJson = (data) => { return JSON.parse(JSON.stringify(data)) };
+const bsonToJson = (data) => {
+    return JSON.parse(JSON.stringify(data));
+};
 const removePassword = (data) => {
     const { password, __v, ...userData } = data;
-    return userData
-}
+    return userData;
+};
 
 function register(req, res, next) {
     const { tel, email, username, password, repeatPassword } = req.body;
 
-    return userModel.create({ tel, email, username, password })
+    return userModel
+        .create({ tel, email, username, password })
         .then((createdUser) => {
             createdUser = bsonToJson(createdUser);
             createdUser = removePassword(createdUser);
 
             const token = utils.jwt.createToken({ id: createdUser._id });
             if (process.env.NODE_ENV === 'production') {
-                res.cookie(authCookieName, token, { httpOnly: true, sameSite: 'none', secure: true })
+                res.cookie(authCookieName, token, {
+                    httpOnly: true,
+                    sameSite: 'none',
+                    secure: true,
+                });
             } else {
-                res.cookie(authCookieName, token, { httpOnly: true })
+                res.cookie(authCookieName, token, { httpOnly: true });
             }
-            res.status(200)
-                .send(createdUser);
+            res.status(200).send(createdUser);
         })
-        .catch(err => {
+        .catch((err) => {
             if (err.name === 'MongoError' && err.code === 11000) {
-                let field = err.message.split("index: ")[1];
-                field = field.split(" dup key")[0];
-                field = field.substring(0, field.lastIndexOf("_"));
+                let field = err.message.split('index: ')[1];
+                field = field.split(' dup key')[0];
+                field = field.substring(0, field.lastIndexOf('_'));
 
-                res.status(409)
-                    .send({ message: `This ${field} is already registered!` });
+                res.status(409).send({
+                    message: `This ${field} is already registered!`,
+                });
                 return;
             }
             next(err);
@@ -46,15 +50,18 @@ function register(req, res, next) {
 function login(req, res, next) {
     const { email, password } = req.body;
 
-    userModel.findOne({ email })
-        .then(user => {
-            return Promise.all([user, user ? user.matchPassword(password) : false]);
+    userModel
+        .findOne({ email })
+        .then((user) => {
+            return Promise.all([
+                user,
+                user ? user.matchPassword(password) : false,
+            ]);
         })
         .then(([user, match]) => {
             if (!match) {
-                res.status(401)
-                    .send({ message: 'Wrong email or password' });
-                return
+                res.status(401).send({ message: 'Wrong email or password' });
+                return;
             }
             user = bsonToJson(user);
             user = removePassword(user);
@@ -62,12 +69,15 @@ function login(req, res, next) {
             const token = utils.jwt.createToken({ id: user._id });
 
             if (process.env.NODE_ENV === 'production') {
-                res.cookie(authCookieName, token, { httpOnly: true, sameSite: 'none', secure: true })
+                res.cookie(authCookieName, token, {
+                    httpOnly: true,
+                    sameSite: 'none',
+                    secure: true,
+                });
             } else {
-                res.cookie(authCookieName, token, { httpOnly: true })
+                res.cookie(authCookieName, token, { httpOnly: true });
             }
-            res.status(200)
-                .send(user);
+            res.status(200).send(user);
         })
         .catch(next);
 }
@@ -75,20 +85,24 @@ function login(req, res, next) {
 function logout(req, res) {
     const token = req.cookies[authCookieName];
 
-    tokenBlacklistModel.create({ token })
+    tokenBlacklistModel
+        .create({ token })
         .then(() => {
             res.clearCookie(authCookieName)
                 .status(204)
                 .send({ message: 'Logged out!' });
         })
-        .catch(err => res.send(err));
+        .catch((err) => res.send(err));
 }
 
 function getProfileInfo(req, res, next) {
     const { _id: userId } = req.user;
 
-    userModel.findOne({ _id: userId }, { password: 0, __v: 0 }) //finding by Id and returning without password and __v
-        .then(user => { res.status(200).json(user) })
+    userModel
+        .findOne({ _id: userId }, { password: 0, __v: 0 }) //finding by Id and returning without password and __v
+        .then((user) => {
+            res.status(200).json(user);
+        })
         .catch(next);
 }
 
@@ -96,8 +110,15 @@ function editProfileInfo(req, res, next) {
     const { _id: userId } = req.user;
     const { tel, username, email } = req.body;
 
-    userModel.findOneAndUpdate({ _id: userId }, { tel, username, email }, { runValidators: true, new: true })
-        .then(x => { res.status(200).json(x) })
+    userModel
+        .findOneAndUpdate(
+            { _id: userId },
+            { tel, username, email },
+            { runValidators: true, new: true }
+        )
+        .then((x) => {
+            res.status(200).json(x);
+        })
         .catch(next);
 }
 
@@ -107,4 +128,4 @@ module.exports = {
     logout,
     getProfileInfo,
     editProfileInfo,
-}
+};
