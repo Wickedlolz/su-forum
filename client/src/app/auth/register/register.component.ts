fe182d4b069d28b3adab9c4366a6190732334178
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-01-29 14:59:30 +0200

    add handle register form

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -5,7 +5,10 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { passwordMatch } from '../utils';
+import { Router } from '@angular/router';
+import { IUserRegisterDto } from 'src/app/core/interfaces/user';
+import { UserService } from 'src/app/core/services/user.service';
+import { emailValidator, passwordMatch } from '../utils';
 
 @Component({
   selector: 'app-register',
@@ -18,19 +21,49 @@ export class RegisterComponent {
     Validators.minLength(5),
   ]);
 
+  get passwordGroup(): FormGroup {
+    return this.registerFormGroup.controls['passwords'] as FormGroup;
+  }
+
   registerFormGroup: FormGroup = this.formBuilder.group({
-    username: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    email: new FormControl('', [Validators.required, emailValidator]),
     tel: new FormControl('', [Validators.required]),
     passwords: new FormGroup({
       password: this.passwordControl,
-      rePassword: new FormControl(null, [passwordMatch(this.passwordControl)]),
+      rePassword: new FormControl(null, [
+        Validators.required,
+        passwordMatch(this.passwordControl),
+      ]),
     }),
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   handleRegister(): void {
-    console.log(this.registerFormGroup.controls);
+    if (this.registerFormGroup.invalid) return;
+
+    const body: IUserRegisterDto = {
+      username: this.registerFormGroup.value.username,
+      email: this.registerFormGroup.value.email,
+      tel: this.registerFormGroup.value.tel,
+      password: this.registerFormGroup.value.passwords.password,
+    };
+
+    this.userService.register(body).subscribe({
+      next: (user) => {
+        this.userService.user = user;
+        this.userService.isLoggedIn = true;
+        this.router.navigate(['/home']);
+      },
+      error: (error) => console.error(error),
+    });
   }
 }
