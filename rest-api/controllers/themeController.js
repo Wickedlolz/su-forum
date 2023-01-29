7bb104d3961dc8bb3997b0ace79ae9273bd3fd5d
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-01-29 15:27:14 +0200

    format code

--- rest-api/controllers/themeController.js
+++ rest-api/controllers/themeController.js
@@ -1,24 +1,26 @@
 const { themeModel } = require('../models');
-const { newPost } = require('./postController')
+const { newPost } = require('./postController');
 
 function getThemes(req, res, next) {
-    themeModel.find()
+    themeModel
+        .find()
         .populate('userId')
-        .then(themes => res.json(themes))
+        .then((themes) => res.json(themes))
         .catch(next);
 }
 
 function getTheme(req, res, next) {
     const { themeId } = req.params;
 
-    themeModel.findById(themeId)
+    themeModel
+        .findById(themeId)
         .populate({
-            path : 'posts',
-            populate : {
-              path : 'userId'
-            }
-          })
-        .then(theme => res.json(theme))
+            path: 'posts',
+            populate: {
+                path: 'userId',
+            },
+        })
+        .then((theme) => res.json(theme))
         .catch(next);
 }
 
@@ -26,10 +28,12 @@ function createTheme(req, res, next) {
     const { themeName, postText } = req.body;
     const { _id: userId } = req.user;
 
-    themeModel.create({ themeName, userId, subscribers: [userId] })
-        .then(theme => {
-            newPost(postText, userId, theme._id)
-                .then(([_, updatedTheme]) => res.status(200).json(updatedTheme))
+    themeModel
+        .create({ themeName, userId, subscribers: [userId] })
+        .then((theme) => {
+            newPost(postText, userId, theme._id).then(([_, updatedTheme]) =>
+                res.status(200).json(updatedTheme)
+            );
         })
         .catch(next);
 }
@@ -37,9 +41,14 @@ function createTheme(req, res, next) {
 function subscribe(req, res, next) {
     const themeId = req.params.themeId;
     const { _id: userId } = req.user;
-    themeModel.findByIdAndUpdate({ _id: themeId }, { $addToSet: { subscribers: userId } }, { new: true })
-        .then(updatedTheme => {
-            res.status(200).json(updatedTheme)
+    themeModel
+        .findByIdAndUpdate(
+            { _id: themeId },
+            { $addToSet: { subscribers: userId } },
+            { new: true }
+        )
+        .then((updatedTheme) => {
+            res.status(200).json(updatedTheme);
         })
         .catch(next);
 }
@@ -49,4 +58,4 @@ module.exports = {
     createTheme,
     getTheme,
     subscribe,
-}
+};
