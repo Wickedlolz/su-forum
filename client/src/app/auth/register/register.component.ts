6f108e33f27786a741b8b4e07fdccc9fcac87dab
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-01-29 14:11:06 +0200

    add form group with all form controlls

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { passwordMatch } from '../utils';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +13,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor() {}
+  passwordControl = new FormControl(null, [
+    Validators.required,
+    Validators.minLength(5),
+  ]);
+
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    tel: new FormControl('', [Validators.required]),
+    passwords: new FormGroup({
+      password: this.passwordControl,
+      rePassword: new FormControl(null, [passwordMatch(this.passwordControl)]),
+    }),
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  handleRegister(): void {
+    console.log(this.registerFormGroup.controls);
+  }
 }
