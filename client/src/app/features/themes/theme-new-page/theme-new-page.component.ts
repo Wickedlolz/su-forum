9579b171464c3b2c8be79a9f7cb35309807da917
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-01-29 15:30:25 +0200

    add handle adding new theme and on cancel navigate to home

--- client/src/app/features/themes/theme-new-page/theme-new-page.component.ts
+++ client/src/app/features/themes/theme-new-page/theme-new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ThemeService } from 'src/app/core/services/theme.service';
 
 @Component({
   selector: 'app-theme-new-page',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./theme-new-page.component.css'],
 })
 export class ThemeNewPageComponent {
-  constructor() {}
+  constructor(private router: Router, private themeService: ThemeService) {}
+
+  handleAddNewTheme(newThemeForm: NgForm): void {
+    console.log(newThemeForm.value);
+    this.themeService.addTheme(newThemeForm.value).subscribe({
+      next: (newTheme) => {
+        this.router.navigate(['/themes']);
+      },
+      error: (error) => console.error(error),
+    });
+  }
+
+  navigateToHome($event: MouseEvent): void {
+    $event.preventDefault();
+    this.router.navigate(['/home']);
+  }
 }
