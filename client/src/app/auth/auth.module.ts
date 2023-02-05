ab6997496b993a6d331b04c9e298f6c9e5ad00c0
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-02-05 15:37:19 +0200

    add email validator directive

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmailValidatorDirective } from './email-validator.directive';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, ProfileComponent],
+  declarations: [LoginComponent, RegisterComponent, ProfileComponent, EmailValidatorDirective],
   imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, FormsModule],
 })
 export class AuthModule {}
