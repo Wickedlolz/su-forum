f4a04e26deb539e432143ebaee440c6feae4b5af
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-18 15:52:18 +0200

    format code

--- rest-api/src/controllers/user.controller.js
+++ rest-api/src/controllers/user.controller.js
@@ -1,37 +1,37 @@
-import { Router } from "express";
-import { isAuth } from "../middlewares/guards.middleware.js";
-import { userService } from "../services/index.js";
+import { Router } from 'express';
+import { isAuth } from '../middlewares/guards.middleware.js';
+import { userService } from '../services/index.js';
 
 const router = Router();
 
-router.get("/profile", isAuth(), async (req, res, next) => {
-  const { _id: userId } = req.user;
+router.get('/profile', isAuth(), async (req, res, next) => {
+    const { _id } = req.user;
 
-  try {
-    const user = await userService.getProfileInfo(userId);
+    try {
+        const user = await userService.getProfileInfo(_id);
 
-    res.json(user);
-  } catch (error) {
-    next(error);
-  }
+        res.json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.put("/profile", isAuth(), async (req, res, next) => {
-  const { _id: userId } = req.user;
-  const { tel, username, email } = req.body;
+router.put('/profile', isAuth(), async (req, res, next) => {
+    const { _id: userId } = req.user;
+    const { tel, username, email } = req.body;
 
-  try {
-    const updatedUser = await userService.editProfileInfo(
-      userId,
-      tel,
-      username,
-      email
-    );
+    try {
+        const updatedUser = await userService.editProfileInfo(
+            userId,
+            tel,
+            username,
+            email
+        );
 
-    res.status(201).json(updatedUser);
-  } catch (error) {
-    next(error);
-  }
+        res.status(201).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default router;
