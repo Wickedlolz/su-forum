1ef87018605aadad36c47bab43d2b53211aa7e3a
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-18 16:16:18 +0200

    fix change to single quotes

--- rest-api/src/middlewares/auth.middleware.js
+++ rest-api/src/middlewares/auth.middleware.js
@@ -1,29 +1,29 @@
-import { authService } from "../services/index.js";
+import { authService } from '../services/index.js';
 
 const authMiddleware = function () {
-  return async (req, res, next) => {
-    const token = req.cookies[process.env.COOKIE_NAME] || undefined;
+    return async (req, res, next) => {
+        const token = req.cookies[process.env.COOKIE_NAME] || undefined;
 
-    if (token) {
-      try {
-        const payload = await authService.validateToken(token);
+        if (token) {
+            try {
+                const payload = await authService.validateToken(token);
 
-        req.user = {
-          email: payload.email,
-          _id: payload._id,
-          username: payload.username,
-          token,
-        };
-      } catch (error) {
-        return res
-          .status(403)
-          .clearCookie(process.env.COOKIE_NAME)
-          .json({ message: "Invalid access token. Please sign in." });
-      }
-    }
+                req.user = {
+                    email: payload.email,
+                    _id: payload._id,
+                    username: payload.username,
+                    token,
+                };
+            } catch (error) {
+                return res
+                    .status(403)
+                    .clearCookie(process.env.COOKIE_NAME)
+                    .json({ message: 'Invalid access token. Please sign in.' });
+            }
+        }
 
-    next();
-  };
+        next();
+    };
 };
 
 export default authMiddleware;
