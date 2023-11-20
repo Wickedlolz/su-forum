e76d4f06ec97b6914ed50781e052c75b41b96905
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-20 18:11:08 +0200

    add getters for can subscribe and is logged in

--- client/src/app/features/themes/theme-item/theme-item.component.ts
+++ client/src/app/features/themes/theme-item/theme-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ITheme } from 'src/app/core/interfaces/theme';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-theme-item',
@@ -8,4 +9,14 @@ import { ITheme } from 'src/app/core/interfaces/theme';
 })
 export class ThemeItemComponent {
   @Input() theme!: ITheme;
+
+  get canSubscribe(): boolean | undefined {
+    return !this.theme.subscribers.includes(this.authService.user!._id);
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  constructor(private readonly authService: AuthService) {}
 }
