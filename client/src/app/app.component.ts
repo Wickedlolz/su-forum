1bcc7ede508d65d7b2f65cfcd8a2af59fe2d9f49
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-20 15:54:06 +0200

    add subscription and on destroy unsubscribe

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
+
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    this.authService.authenticate().subscribe({
+    this.subscription = this.authService.authenticate().subscribe({
       next: (user) => {
         this.authService.user = user;
         this.authService.isLoggedIn = true;
       },
       error: (error) => console.error(error),
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
