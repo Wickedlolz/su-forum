63f1a419da373359617156db46130e3807cedf83
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-20 17:15:58 +0200

    fix: save theme before create post

--- rest-api/src/services/theme.service.js
+++ rest-api/src/services/theme.service.js
@@ -1,35 +1,38 @@
-import Theme from "../models/theme.model.js";
-import { createPost } from "./post.service.js";
+import Theme from '../models/theme.model.js';
+import { createPost } from './post.service.js';
 
 export const getThemes = async () => {
-  const themes = await Theme.find({}).populate("userId");
-  return themes;
+    const themes = await Theme.find({}).populate('userId');
+    return themes;
 };
 
 export const getThemeById = async (themeId) => {
-  const theme = await Theme.findById(themeId).populate({
-    path: "posts",
-    populate: {
-      path: "userId",
-    },
-  });
+    const theme = await Theme.findById(themeId).populate({
+        path: 'posts',
+        populate: {
+            path: 'userId',
+        },
+    });
 
-  return theme;
+    return theme;
 };
 
 export const createTheme = async (themeName, userId, postText) => {
-  const theme = new Theme({ themeName, userId, subscribers: [userId] });
-  const updatedTheme = await createPost(postText, userId, theme._id);
+    const theme = new Theme({ themeName, userId, subscribers: [userId] });
 
-  return updatedTheme;
+    await theme.save();
+
+    const updatedTheme = await createPost(postText, userId, theme._id);
+
+    return updatedTheme;
 };
 
 export const subscribe = async (themeId, userId) => {
-  const updatedTheme = await Theme.findByIdAndUpdate(
-    { _id: themeId },
-    { $addToSet: { subscribers: userId } },
-    { new: true }
-  );
+    const updatedTheme = await Theme.findByIdAndUpdate(
+        { _id: themeId },
+        { $addToSet: { subscribers: userId } },
+        { new: true }
+    );
 
-  return updatedTheme;
+    return updatedTheme;
 };
