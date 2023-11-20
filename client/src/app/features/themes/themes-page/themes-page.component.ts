aa4bff2345d85acfb48e5cfc24ee7a9c0a01b0c2
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-20 18:11:40 +0200

    add getter for is logged in

--- client/src/app/features/themes/themes-page/themes-page.component.ts
+++ client/src/app/features/themes/themes-page/themes-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-themes-page',
   templateUrl: './themes-page.component.html',
-  styleUrls: ['./themes-page.component.css']
+  styleUrls: ['./themes-page.component.css'],
 })
 export class ThemesPageComponent {
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
 
+  constructor(private readonly authService: AuthService) {}
 }
