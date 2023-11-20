522d867a26d5aca1017ca4e4ee722497c33ad03c
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-20 16:21:06 +0200

    add subscripiton

--- client/src/app/features/themes/aside/aside.component.ts
+++ client/src/app/features/themes/aside/aside.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IPost } from 'src/app/core/interfaces/post';
 import { PostService } from 'src/app/core/services/post.service';
 
@@ -7,15 +8,16 @@ import { PostService } from 'src/app/core/services/post.service';
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css'],
 })
-export class AsideComponent implements OnInit {
+export class AsideComponent implements OnInit, OnDestroy {
   posts!: IPost[];
   isLoading: boolean = true;
+  subscription!: Subscription;
 
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.postService.loadPosts$(5).subscribe({
+    this.subscription = this.postService.loadPosts$(5).subscribe({
       next: (posts) => {
         this.posts = posts;
         this.isLoading = false;
@@ -25,5 +27,9 @@ export class AsideComponent implements OnInit {
         this.isLoading = false;
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
