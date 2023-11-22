13b08e508093135e32aa8f79c2003545b5f904eb
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-22 17:18:41 +0200

    fix: change http method to get for logout

--- rest-api/src/controllers/auth.controller.js
+++ rest-api/src/controllers/auth.controller.js
@@ -1,48 +1,47 @@
-import { Router } from "express";
-import { authService } from "../services/index.js";
+import { Router } from 'express';
+import { authService } from '../services/index.js';
 
 const router = Router();
 
-router.post("/register", async (req, res, next) => {
-  const { tel, email, username, password } = req.body;
+router.post('/register', async (req, res, next) => {
+    const { tel, email, username, password } = req.body;
 
-  try {
-    const user = await authService.register(tel, email, username, password);
+    try {
+        const user = await authService.register(tel, email, username, password);
 
-    const token = await authService.createToken(user);
+        const token = await authService.createToken(user);
 
-    res.cookie(process.env.COOKIE_NAME, token, {
-      httpOnly: true,
-    });
+        res.cookie(process.env.COOKIE_NAME, token, {
+            httpOnly: true,
+        });
 
-    res.status(201).json(user);
-  } catch (error) {
-    next(error);
-  }
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
+router.post('/login', async (req, res, next) => {
+    const { email, password } = req.body;
 
-  try {
-    const user = await authService.login(email, password);
-    const token = await authService.createToken(user);
+    try {
+        const user = await authService.login(email, password);
+        const token = await authService.createToken(user);
 
-    res.cookie(process.env.COOKIE_NAME, token, {
-      httpOnly: true,
-    });
+        res.cookie(process.env.COOKIE_NAME, token, {
+            httpOnly: true,
+        });
 
-    res.json(user);
-  } catch (error) {
-    next(error);
-  }
+        res.json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.post("/logout", async (req, res) => {
-  res
-    .clearCookie(process.env.COOKIE_NAME)
-    .status(204)
-    .json({ message: "Logged out!" });
+router.get('/logout', (req, res) => {
+    res.clearCookie(process.env.COOKIE_NAME)
+        .status(204)
+        .json({ message: 'Logged out!' });
 });
 
 export default router;
