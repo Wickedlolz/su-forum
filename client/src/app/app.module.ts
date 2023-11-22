e4c6dd8bfe1d1d062f225b11a5b598ab2c549a14
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-22 17:00:38 +0200

    import auth module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CoreModule } from './core/core.module';
 import { ThemesModule } from './features/themes/themes.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './features/pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { PagesModule } from './features/pages/pages.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule.forRoot(),
+    AuthModule,
     ThemesModule,
     SharedModule,
     PagesModule,
