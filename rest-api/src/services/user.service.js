7a0cd8c52fbce39ec06158ce3de6a4222c6913ca
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-11-29 11:21:43 +0200

    fix: return updated user without password

--- rest-api/src/services/user.service.js
+++ rest-api/src/services/user.service.js
@@ -1,20 +1,22 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 
 export const getProfileInfo = async (userId) => {
-  const user = await User.findOne(
-    { _id: userId },
-    { password: 0, __v: 0 }
-  ).lean();
+    const user = await User.findOne(
+        { _id: userId },
+        { password: 0, __v: 0 }
+    ).lean();
 
-  return user;
+    return user;
 };
 
 export const editProfileInfo = async (userId, tel, username, email) => {
-  const updatedUser = await User.findOneAndUpdate(
-    { _id: userId },
-    { tel, username, email },
-    { runValidators: true, new: true }
-  );
+    const updatedUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { tel, username, email },
+        { runValidators: true, new: true }
+    ).lean();
 
-  return updatedUser;
+    const { password, ...userData } = updatedUser;
+
+    return userData;
 };
