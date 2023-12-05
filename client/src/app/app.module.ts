74dc5f8328c77b2aea78e67a21e063f864aa2961
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-12-05 18:27:19 +0200

    remove imports of auth and thems modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { CoreModule } from './core/core.module';
 import { ThemesModule } from './features/themes/themes.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './features/pages/pages.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,8 +16,6 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     HttpClientModule,
     CoreModule.forRoot(),
-    AuthModule,
-    ThemesModule,
     SharedModule,
     PagesModule,
   ],
