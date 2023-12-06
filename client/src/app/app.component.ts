dc9a1a10fe43418ce674b9a99496ae329cf3ab50
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-12-06 18:02:34 +0200

    remove unused import

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
-import { EMPTY, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
