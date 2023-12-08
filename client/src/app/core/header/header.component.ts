4368af616f0f6379fced184d0034c1e4199c5a23
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-12-08 16:21:40 +0200

    add logic for show notification

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { IUser } from '../interfaces/user';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MessageBusService } from '../services/message-bus.service';
+import { Subscription } from 'rxjs';
+import { MessageType } from '../interfaces/message';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   currentUser$ = this.authService.currentUser$;
   isLoggedIn$ = this.authService.isLoggedIn$;
+  private subscription!: Subscription;
+  message!: string;
+  isMessageError!: boolean;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private messageBus: MessageBusService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.messageBus.onNewMessage$.subscribe(
+      (newMessage) => {
+        this.message = newMessage?.text || '';
+        this.isMessageError = newMessage?.type === MessageType.Error;
+
+        if (this.message) {
+          setTimeout(() => {
+            this.messageBus.clear();
+          }, 4000);
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   handleLogout($event: MouseEvent): void {
     $event.preventDefault();
