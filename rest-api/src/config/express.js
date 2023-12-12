7d1af165087abec4acde3dd0fe34ea109eda5f1b
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-12-12 11:56:02 +0200

    fix: add limit to json to handle bigger image ✅

--- rest-api/src/config/express.js
+++ rest-api/src/config/express.js
@@ -7,7 +7,7 @@ import { v2 as cloudinary } from 'cloudinary';
 const whitelist = ['http://localhost:5000', 'http://localhost:4200'];
 
 const epxressConfig = (app) => {
-    app.use(express.json());
+    app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser(process.env.COOKIE_SECRET));
     app.use(
