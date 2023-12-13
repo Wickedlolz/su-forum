3f219d9c223f7022d1513072503c8f3e879799b8
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-12-13 12:15:09 +0200

    reorder imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './features/pages/pages.module';
-import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
