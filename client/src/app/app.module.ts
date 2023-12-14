178e71b5e3470df2e8959fa04b0f8abac48f37e2
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2023-12-14 20:25:09 +0200

    feat: Added ngrx devtools

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { PagesModule } from './features/pages/pages.module';
     CoreModule.forRoot(),
     SharedModule,
     PagesModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
   bootstrap: [AppComponent],
